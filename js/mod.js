680d6bd8340344e17a6fdc0f5687095e8d6cc52e
bumpy <125849993+sdbumpy@users.noreply.github.com>
2024-10-06 15:47:56 -0500

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,26 +1,25 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
-	pointsName: "points",
+	name: "The Layers Tree",
+	id: "tmt_layers",
+	author: "bumpy",
+	pointsName: "progress",
 	modFiles: ["layers.js", "tree.js"],
 
 	discordName: "",
 	discordLink: "",
 	initialStartPoints: new Decimal (10), // Used for hard resets and new players
-	offlineLimit: 1,  // In hours
+	offlineLimit: 1/6,  // In hours
 }
 
 // Set your version in num and name
 let VERSION = {
 	num: "0.0",
-	name: "Literally nothing",
+	name: "First Edition",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
 	<h3>v0.0</h3><br>
-		- Added things.<br>
-		- Added stuff.`
+		- Added points, prestige points, power tiers, and super prestige points.<br>`
 
 let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
 
@@ -34,7 +33,7 @@ function getStartPoints(){
 
 // Determines if it should show points/sec
 function canGenPoints(){
-	return true
+	return hasUpgrade('p', 11)
 }
 
 // Calculate points/sec!
@@ -42,8 +41,18 @@ function getPointGen() {
 	if(!canGenPoints())
 		return new Decimal(0)
 
-	let gain = new Decimal(1)
-	return gain
+	let mult = new Decimal(1)
+	mult = mult.mul(buyableEffect('p', 11))
+	mult = mult.mul(buyableEffect('p', 13))
+	mult = mult.mul(buyableEffect('pr', 12))
+	if (hasUpgrade('pr', 11)) mult = mult.mul(upgradeEffect('pr', 11))
+	if (hasUpgrade('pr', 13)) mult = mult.mul(upgradeEffect('pr', 13))
+	if (hasMilestone('pt', 0)) mult = mult.mul(new Decimal(1.5).pow(player.pt.points))
+	if (hasUpgrade('spr', 42)) mult = mult.mul(upgradeEffect('spr', 42))
+	if (hasUpgrade('spr', 51)) mult = mult.mul(upgradeEffect('spr', 51))
+	
+	if (hasUpgrade('spr', 11)) mult = mult.pow(upgradeEffect('spr', 11))
+	return mult
 }
 
 // You can add non-layer related variables that should to into "player" and be saved here, along with default values
@@ -52,11 +61,12 @@ function addedPlayerData() { return {
 
 // Display extra things at the top of the page
 var displayThings = [
+	"<span style='color:grey'>endgame: 30 total prestige tokens</span>"
 ]
 
 // Determines when the game "ends"
 function isEndgame() {
-	return player.points.gte(new Decimal("e280000000"))
+	return player.points.gte(new Decimal("e3003"))
 }
 
 
@@ -70,7 +80,7 @@ var backgroundStyle = {
 
 // You can change this if you have things that can be messed up by long tick lengths
 function maxTickLength() {
-	return(3600) // Default is 1 hour which is just arbitrarily large
+	return(600) // Default is 1 hour which is just arbitrarily large
 }
 
 // Use this if you need to undo inflation from an older version. If the version is older than the version that fixed the issue,
